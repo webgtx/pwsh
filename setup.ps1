686be056826ea7ca79cde9ac92bd297e0d226207
webgtx <dxv1d@tuta.io>
2022-07-19 15:28:45 +0200

    Update: added powershell 7.2.5 support

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,18 @@
-cp -Recurse * $env:USERPROFILE\Documents\WindowsPowerShell\
+$pwsh_cfg = "$env:USERPROFILE\.config\powershell\user_profile.ps1"
+$winpwsh_pwd = "$env:USERPROFILE\Documents\PowerShell"
+$pwsh_pwd = "$env:USERPROFILE\Documents\WindowsPowerShell"
+
+Write-Output "Script Variables Check",
+  $pwsh_cfg,
+  $winpwsh_pwd,
+  $pwsh_pwd
+
+Start-Job -ScriptBlock {
+  mkdir $pwsh_pwd,
+        $winpwsh_pwd,
+        $env:USERPROFILE\.config\powershell
+}
+
+Copy-Item -Recurse * $env:USERPROFILE\.config\powershell
+Write-Output ". $pwsh_cfg" > $winpwsh_pwd\profile.ps1 
+Copy-Item $winpwsh_pwd\profile.ps1 $pwsh_pwd\profile.ps1
