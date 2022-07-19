a6a9b5c5acc70425e267370a4ddbe34f52af7302
webgtx <dxv1d@tuta.io>
2022-07-19 15:45:37 +0200

    Hotfix: fixed prompt shell

--- profile.ps1
+++ profile.ps1
@@ -3,8 +3,10 @@ echo "Hello again, David :)"
 
 # Aliases
 sal vim nvim
-curl
+
 
 # Prompt
 Import-Module posh-git
 Import-Module Terminal-Icons
+oh-my-posh prompt -c $env:USERPROFILE\.config\powershell\theme.json init pwsh 
+  | Invoke-Expression
