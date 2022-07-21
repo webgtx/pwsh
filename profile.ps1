186e66601dfdf8f81a3a075eb323c4931b974dc7
webgtx <dxv1d@tuta.io>
2022-07-22 01:12:08 +0200

    Update: added PSReadLine support

--- profile.ps1
+++ profile.ps1
@@ -2,11 +2,13 @@
 echo "Hello again, David :)"
 
 # Aliases
-sal vim nvim
-
+Set-Alias vim nvim
 
 # Prompt
 Import-Module posh-git
 Import-Module Terminal-Icons
+
 oh-my-posh prompt -c $env:USERPROFILE\.config\powershell\theme.json init pwsh 
   | Invoke-Expression
+
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
--- requirements.ps1
+++ requirements.ps1
@@ -1,4 +1,6 @@
 scoop install gsudo git oh-my-posh
 
+Install-Module PSReadLine -Scope CurrentUser -Force
 Install-Module posh-git -Scope CurrentUser -Force
 sudo Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+
